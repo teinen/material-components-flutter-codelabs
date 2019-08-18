8c57c2ee8a3db4de21c55fab9a227d2f2fdd5562
Akira Kanno <teinen.github@gmail.com>
2019-08-18 22:05:24 +0900

    Add primary icon theme

--- mdc_100_series/lib/app.dart
+++ mdc_100_series/lib/app.dart
@@ -66,7 +66,7 @@ ThemeData _buildShrineTheme() {
     textTheme: _buildShrineTextTheme(base.textTheme),
     primaryTextTheme: _buildShrineTextTheme(base.primaryTextTheme),
     accentTextTheme: _buildShrineTextTheme(base.accentTextTheme),
-    // TODO: Add the icon themes (103)
+    primaryIconTheme: base.iconTheme.copyWith(color: kShrineBrown900),
     // TODO: Decorate the inputs (103)
   );
 }
